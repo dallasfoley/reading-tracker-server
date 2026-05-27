9f60b3e7b9b8f6f6af820ed46de1e94c188c1975
dallasfoley <dallastfoley@gmail.com>
2026-05-27 17:08:32 -0400

    started user

--- src/main/java/com/dtf/reading_tracker_server/user/User.java
+++ src/main/java/com/dtf/reading_tracker_server/user/User.java
@@ -1,5 +1,45 @@
 package com.dtf.reading_tracker_server.user;
 
+import com.dtf.reading_tracker_server.user.enums.Provider;
+import com.dtf.reading_tracker_server.user.enums.Role;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Provider provider;
+
+    @Column
+    private String avatarUrl;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
 
 }
