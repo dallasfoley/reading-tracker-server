2521e8cc5ad058d0c1f0cd683a02b3cfe7e069c7
dallasfoley <dallastfoley@gmail.com>
2026-07-01 04:36:59 -0400

    allow patch http method

--- src/main/java/com/dtf/reading_tracker_server/shared/config/CorsConfig.java
+++ src/main/java/com/dtf/reading_tracker_server/shared/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000"
         ));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
         config.setExposedHeaders(List.of("WWW-Authenticate"));
